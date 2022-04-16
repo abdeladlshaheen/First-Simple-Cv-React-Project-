1a8b5bd66cc465bfdc9a28a7431002fa823e4175
abdeladlshaheen <abdeladlshaheen@gmail.com>
2022-04-16 12:37:37 +0200

    add linkedIn link

--- src/components/heroComponent.js
+++ src/components/heroComponent.js
@@ -6,7 +6,7 @@ function Hero(props)
                 <div className="col-4 text-white d-flex flex-column justify-content-center align-items-center">
                     <h2>{props.data.userName}</h2>
                     <p className="fs-4">Full Stack Web Developer</p>
-                    <a className="btn btn-dark border border-light w-50">Show my LinkedIn</a>
+                    <a className="btn btn-dark border border-light w-50" href="https://www.linkedin.com/in/abdeladl-shaheen-%F0%9F%87%AA%F0%9F%87%AC-a313b8164/" target="_blank">Show my LinkedIn</a>
                 </div>
                 <div className="col-8">
                     <img className="opacity-25 w-100" src={process.env.PUBLIC_URL + '/picture.jpg'} />
